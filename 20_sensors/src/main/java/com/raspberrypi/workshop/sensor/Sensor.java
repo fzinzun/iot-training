417651b39ccd9e3ef85fafe7503f4f27ff0a8a42
Francisco <francisco.zinzun@ge.com>
2017-04-04 21:56:13 -0500

    Remove Unused folders

--- 20_sensors/src/main/java/com/raspberrypi/workshop/sensor/Sensor.java
+++
@@ -1,8 +0,0 @@
-package com.raspberrypi.workshop.sensor;
-
-import java.beans.PropertyChangeListener;
-
-public interface Sensor {
-	
-	public void addListener(PropertyChangeListener sensorListener);
-}
