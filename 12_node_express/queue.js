e957c159746af923e418666dbdaf6d1478176e9b
Ricardo Uribe <ricardoauribe@gmail.com>
2017-05-10 21:30:11 -0500

    [RUL 10-MAY-2017] Conexión a la cola de rabbitmp en proyecto 12

--- 12_node_express/queue.js
+++ 12_node_express/queue.js
@@ -3,39 +3,38 @@
  $(document).ready(function() {
   if(window.WebSocket) {
     var client, destination;
-
-      var url = "ws://192.168.1.72:15674/ws";
-      var login = "admin";
-      var passcode = "admin";
-      destination = "/exchange/simple.exchange";
-      client = Stomp.client(url);
+    var url = "ws://192.168.1.73:15674/ws";
+    var login = "guest";
+    var passcode = "guest";
 
 
-      // the client is notified when it is connected to the server.
-      client.connect(login, passcode, function(frame) {
-        client.debug("connected to Stomp");
-        client.subscribe(destination, function(message) {
-          $("#messages").append("<p>" + message.body + "</p>\n");
-          var toasts = document.getElementById('toasts');
-          var properties = {
-              duration: 3000,
-              actionText: '',
-              showCancel: true,
-              disableSwipe: false,
-              fitBottom: true,
-              onAction: function() { feedback2.innerText = "onAction()"; },
-              onCancel: function() { feedback2.innerText = "onCancel()"; }
+    destination = "/exchange/simple.exchange";
+    client = Stomp.client(url);
 
-          }
+    // the client is notified when it is connected to the server.
+    client.connect(login, passcode, function(frame) {
 
-          var timerID = setInterval(function() {
-              toasts.showSuccessToast(message.body, properties);
-          }, 60 * 1000);
+      client.debug("connected to Stomp");
 
-        });
+      client.subscribe(destination, function(message) {
+
+        console.log(message);
+        let color = "defaultToast";
+        let messageBody = JSON.parse(message.body);
+        let messageId = messageBody.id;
+        let messageType = messageBody.type;
+        let messageValue = messageBody.value;
+
+        //Format the message and color
+        if(messageType == "temperature"){
+            color = "redToast"
+        }
+
+        $("#messages").append("<paper-toast class='" + color + "' text='Message Type: " + messageType + " | Value: " + messageValue + " | Id: " +messageId + "' opened></paper-toast>");
+
       });
 
-
+    });
   } else {
     $("#connect").html("\
         <h1>Get a new Web Browser!</h1>\
